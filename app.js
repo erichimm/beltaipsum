73083379698bb6dae573c682acf7c3e0465f9ade
Eric Himmelblau <erichimmelblau@gmail.com>
2019-06-22 14:15:56 -0500

    add and configure app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const   express =       require('express'),
+        app =           express(),
+        bodyParser =    require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.set('veiw engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Starting server on port 3000');
+});
