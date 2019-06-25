9f3b8958f7f7d1cf52433c1cd7c606cb5255849c
Eric Himmelblau <erichimmelblau@gmail.com>
2019-06-25 18:25:00 -0500

    add makeSentance function to generator. convert belter_liberary.txt to lowercase.

--- generator.js
+++ generator.js
@@ -9,7 +9,12 @@ function generate(options) {
 
     var words = getWords(options.genType);
 
-    return JSON.stringify(options);
+    var text = '';
+    for(var i=0; i<options.paragraphs; i++){
+        text += makeParagraph(words);
+    }
+
+    return JSON.stringify(text);
 }
 
 function getWords(type) {
@@ -25,6 +30,38 @@ function getWords(type) {
                             .toString('utf-8').split(os.EOL);
         return belterWords;
     }
+}
+
+function makeSentance(words, length) {
+    var sentance = '';
+
+    for(var i=0; i<length; i++) {
+        // pick a random word from [0, words.length)
+        let word = words[Math.floor(Math.random() * words.length)];
+        // if first word, capitalize
+        if(i === 0) {
+            word = word.charAt(0).toUpperCase() + word.slice(1);
+        }
+        sentance += word;
+
+        // 10% chance to add a comma in the middle of the sentance
+        if(i>3 && i<length-3) {
+            if(Math.random()*10 < 1) {
+                sentance += ',';
+            }
+        }
+        if(i === length-1) {
+            sentance += '.';
+        } else {
+            sentance += ' ';
+        }
+    }
+
+    return sentance;
+}
+
+function makeParagraph(words) {
+    return makeSentance(words, 14) + ' ';
 }
 
 module.exports = { generate };
