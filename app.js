939d2d33459f516e94b7eaa08dae63d8a6a451f3
Eric Himmelblau <erichimmelblau@gmail.com>
2019-06-25 15:11:19 -0500

    add ipsum argument to view

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const   express =       require('express'),
         app =           express(),
         bodyParser =    require('body-parser');
 
+const   generator =     require('./generator.js');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log(req.query.options);
-    res.render('index');
+    // console.log(req.query.options);
+    res.render('index', {ipsum: generator.generate(req.query.options)});
 });
 
 app.listen(process.env.PORT || 3000, () => {
