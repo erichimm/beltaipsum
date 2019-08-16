02b416bbffb3f672c020dd25a1ce33e28467089e
Eric Himmelblau <erichimmelblau@gmail.com>
2019-08-15 22:14:45 -0500

    Compleatly overhaul frontend to use a minimalist design style

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get('/api/belta', (req, res) => {
     res.json(generatedText);
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('Starting server on port 3000');
 });
