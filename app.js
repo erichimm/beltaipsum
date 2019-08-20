d90ec2f06145c19c927e4394b4ef71bf0fdbe9c7
Eric Himmelblau <erichimmelblau@gmail.com>
2019-08-20 15:29:18 -0500

    change defalt port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get('/api/belta', (req, res) => {
     res.json(generatedText);
 })
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Starting server on port 3000');
 });
